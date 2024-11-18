d5ba4021b654c2807689582c5c97c2b993610c04
Marius Szabo <szabomarius@gmail.com>
2024-11-18 12:11:38 +0200

    tidy everything up

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
     splitting: false,
     sourcemap: true,
     clean: true,
+    minify: true,
+    terserOptions: {
+        mangle: {
+            properties: {
+                regex: /^_/,
+            },
+            keep_classnames: false,
+            keep_fnames: false,
+            eval: false,
+        },
+        compress: {
+            drop_console: true,
+            keep_classnames: false,
+            side_effects: true,
+        },
+    },
 });
